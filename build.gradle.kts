5e5477fc5a6913001929ee9b005f7860bd8c7cd6
mibers <woodrow.conran@gmail.com>
2025-02-17 22:13:12 +0000

    updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,22 +4,20 @@ plugins {
 }
 
 group = "me.mibers"
+
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
     testImplementation(kotlin("test"))
     implementation("net.minestom:minestom-snapshots:7589b3b655")
     implementation("org.luaj:luaj-jse:3.0.1")
+    implementation("party.iroiro.luajava:luajava:4.0.2")
+    implementation("party.iroiro.luajava:luajit:4.0.2")
+    runtimeOnly("party.iroiro.luajava:luajit-platform:4.0.2:natives-desktop")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
+tasks.test { useJUnitPlatform() }
 
-kotlin {
-    jvmToolchain(21)
-}
+kotlin { jvmToolchain(21) }
